448f8ef3bee51a2052e5f8fadf0669c873155499
Allie <allieroblee@cyberresilience.com>
2023-11-12 17:05:30 -0500

    Added temporary camera target change test
    Added temporary camera target change test

--- game_manager/game_manager.gd
+++ game_manager/game_manager.gd
@@ -5,6 +5,7 @@ static var save_file_path = "user://save/"
 static var save_file_name = "save.tres"
 
 @export var PlayerScene: PackedScene
+@export var NPC_Scene: PackedScene
 @export var WorldScene: PackedScene
 
 @export var ui: UI
@@ -15,12 +16,19 @@ signal toggle_game_paused()
 signal save_game_signal()
 
 var player: Player
+var npc: NPC
 var world: World
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_verify_save_directory(save_file_path)
 	connect("save_game_signal",save_game)
+
+func _input(event : InputEvent):
+	if event.is_action_pressed("ui_focus_next"):
+		camera.change_target(npc)
+	if event.is_action_pressed("ui_focus_prev"):
+		camera.change_target(player)
 
 func new_game():
 	#Init World
@@ -32,6 +40,10 @@ func new_game():
 	player = PlayerScene.instantiate()
 	world.add_child(player)
 	player.position = Vector2(0,0)
+	
+	npc = NPC_Scene.instantiate()
+	world.add_child(npc)
+	npc.position = Vector2(-916,150)
 	
 	#Init Camera
 	camera.change_target(player)
